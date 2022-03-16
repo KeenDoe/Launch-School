7afb0b18bf4869664c14b5f36fef51a4e541daa1
Kenji <keendashoota@gmail.com>
2022-03-15 21:11:30 -0400

    First section of Ruby book's exercises

---
+++ the_basics/kenji.rb
@@ -0,0 +1,59 @@
+# this is question 1
+a = "Mckeen"
+
+x = " Doe"
+
+a += x
+puts a
+
+
+
+
+# this is question 2
+thousands = 5678 / 1000
+hundreds = 5678 % 1000 / 100
+tens = 5678 % 100 / 10
+ones = 5678 % 10
+
+puts "thousands: #{thousands}"
+puts "hundreds: #{hundreds}"
+puts "tens: #{tens}"
+puts "ones: #{ones}"
+#this is question 3
+movies = { :v_for_vendetta => 2005, 
+    :black_panther => 2018,
+    :my_hero_academia_world_heroes_mission => 2021
+
+}
+
+puts movies[:v_for_vendetta]
+puts movies[:black_panther]
+puts movies [:my_hero_academia_world_heroes_mission]
+
+#this is question 4
+dates = [2005, 2018, 2021]
+puts dates[0]
+puts dates[1]
+puts dates[2]
+
+#this is question 5
+puts 5 * 4 * 3 * 2 * 1
+puts 6 * 5 * 4 * 3 * 2 * 1
+puts 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+
+
+#this is question 6
+val1 = 45.0
+val2 = 250.5
+val3 = 375.8
+val4 = 630.5
+
+
+puts Math.sqrt(val1)
+puts Math.sqrt(val2)
+puts Math.sqrt(val3)
+puts Math.sqrt(val4)
+
+#this is question 7 : The error message is saying they have an unexpected parentheses the program is expecting a curley bracket 
